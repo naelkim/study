196b0fb99af715fe81609e7064b50e25f1d7e49a
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-07-01 19:12:41 +0900

    Add files via upload

---
+++ books/jump to python/chapter1/chapter1.py
@@ -0,0 +1,62 @@
+1+1
+# 2
+
+import sys
+
+1+2
+# 3
+
+3 / 2.4
+# 1.25
+
+a = 1
+b = 2
+a + b
+# 3
+
+a = 'Python'
+print(a)
+# Python
+
+a = 'Python'
+a
+# Python
+
+
+# if
+a = 3
+if a > 1:
+    print('a is greater than 1')
+
+# a is greater than 1
+
+
+# for 
+for a in [1, 2, 3]:
+    print(a)
+
+# 1
+# 2
+# 3
+
+
+# while 
+i = 0 
+while i < 3:
+    i = i + 1
+    print(i)
+
+# 1
+# 2
+# 3
+
+
+# function
+def add(a, b):
+    return a + b
+
+add(3, 4)
+# 7
+
+
+
