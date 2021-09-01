78cec33c7d361b96a2962577a81dbae38012d7a9
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-09-01 17:25:06 +0900

    Add files via upload

---
+++ Algorithm/built-in function/Week1.R
@@ -0,0 +1,55 @@
+## 단축키(shortcut) 정리 ---- 
+# 1. 책갈피(----) [뺄셈기호 4개 연속]
+# 2. 책갈피 목록 보기  : Ctrl + Shift + O
+# 3. 주석(#)           : Ctrl + Shift + C [맨앞의 주석 만들고 해제하기]
+# 4. 할당연산자(<-)    : Alt  + -
+
+a <- 1
+a
+
+b <- c('a', 'b', 'c')
+
+c <- data.frame(ID = c(1, 2, 3), 
+                name = b)
+
+class(c)
+
+
+# 5. 파이프연산자(%>%) : Ctrl + Shift + M
+
+
+# 6. 줄 복사           : Alt  + Shift + 아래(위)화살표
+
+# 7. 줄 이동           : Alt          + 아래(위)화살표 
+# 8. 줄 삭제           : Ctrl + D
+# 9. 다중커서 지정     : Ctrl + Alt   + 아래(위)화살표 [아래 코드 연습, 해제는 Esc]
+# 안녕하세요? Shortcut을 배워봅시다.
+# 안녕하세요? Shortcut을 배워봅시다.
+# 안녕하세요? Shortcut을 배워봅시다.
+# 안녕하세요? Shortcut을 배워봅시다.
+# 안녕하세요? Shortcut을 배워봅시다.
+# 10. 같은단어 모두 지정 : Ctrl + Alt  + K [똑같은 단어를 찾아 한번에 수정 가능!]
+# 11. 코드 블록 잡기    : Shift + 방향키 
+# 12. 예쁘게 줄맞추기   : Ctrl + Shift + A [아래 코드 연습]  
+myfun <- function(var1) {
+  a <- var1
+  b <- a / 3
+  plot(a, b)
+} 
+myfun(1:10)
+
+
+
+# 13. 새로운 스크립트 창 열기             : Ctrl + Shift + N  
+# 14. 화면 안에서 새로운 스크립트 창 열기 : Ctrl + F7
+# 15. 9개 각각의 화면 확대/축소           : Ctrl + Shift + 1~9 
+# 16. 9개 각각의 화면 이동                : Ctrl         + 1~9 
+# 17. 전체 화면 확대 : Ctrl + Shift + +
+# 18. 전체 화면 축소 : Ctrl         + -
+# 19. 책갈피, function문, for문, if문 접기: Alt  + 0 [숫자0]
+# 20. 책갈피, function문, for문, if문 펴기: Alt  + Shift + O
+
+## 단축키 연습 ----
+# 더 많은 단축키는 Alt + Shift + K에서 확인 [해제는 항상 Esc]
+# Tools > Modify Keyboard Shortcuts에서 본인만의 단축키 생성 가능!  
+
